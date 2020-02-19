f0da4b54befcae1c0722e720d1d8ed9565e870df
wuliwux <christian.mueller@xcore.at>
2020-02-19 19:19:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function startAdapter(options) {
     // you have to call the adapter function and pass a options object
     // name has to be set and has to be equal to adapters folder name and main file name excluding extension
     // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.resol.0
-    var adapter = utils.adapter(options);
+    adapter = utils.adapter(options);
 
     var ctx = {
         headerSet: null,
